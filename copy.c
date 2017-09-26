174107263c62e98d082662fa6a5926bf0505942a
Adrián Sacristán Ibáñez <adriansacristan1993@gmail.com>
2017-09-26 12:26:48 +0200

    Copy all bytes from file to other file

---
+++ copy.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+int main(int arg, char *args[]){
+	int bytes = 100;
+	char *buf[100];
+	
+	int fd = open("jesusgil.jpg",O_RDONLY, S_IREAD);
+	int val_r = 0;
+
+	int fd2 = open("destino.jpg",O_WRONLY, S_IWRITE);
+
+	do {
+		val_r = read(fd,buf,bytes);
+		printf("val_r = %d\n",val_r);
+
+		if (val_r == -1){
+			write(1,"ERROR\n",bytes);
+			close(fd);
+			close(fd2);
+			_exit(0);
+		}
+		write(fd2,buf,val_r);
+	} while(val_r > 0);
+
+	close(fd);
+	close(fd2);
+
+    return 0;
+}
